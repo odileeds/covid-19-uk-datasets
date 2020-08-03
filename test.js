0926291c16f744d3acbd8f7ed03d6c2a2702b3d9
Giles Dring <giles.dring@odileeds.org>
2020-08-03 12:08:38 +0100

    Remove test

--- test.js
+++
@@ -1,9 +0,0 @@
-const { metadata } = require('./src/metadataManager');
-
-async function doIt() {
-  await metadata.readMetadata();
-  metadata.compress();
-  await metadata.writeMetadata();
-}
-
-doIt();
